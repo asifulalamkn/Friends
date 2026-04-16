836d0b939deffcef8fd3c3adefb85fb9748070a1
asifulalamkn <asifulalamkn@gmail.com>
2026-04-16 23:30:28 +0600

    statsSolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,10 @@ const Navbar = () => {
             <span className="hidden sm:inline text-[14px] md:text-[15px]">Timeline</span>
           </NavLink>
           
-          
+          <NavLink to="/stats" className={linkClass}>
+            <FiBarChart2 className="text-lg md:text-xl" />
+            <span className="hidden sm:inline text-[14px] md:text-[15px]">Stats</span>
+          </NavLink>
         </div>
 
       </div>
